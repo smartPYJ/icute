2dce205b951e49f57ecfb222dc6243813cb1585c
smartPyj <uchemonday01@gmail.com>
2020-04-30 03:11:31 +0100

    initializing flask

---
+++ application.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'hello world'
